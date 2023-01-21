b213df8fbfe96e98c9f8fc2d676bf223c5c070d2
Enthys <asenj.mihaylov@gmail.com>
2023-01-21 11:01:07 +0200

    feat: setup of global validation pipe and .env file read

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(ValidationPipe);
+
+  if (process.env.APP_MODE === 'development') {
+    SwaggerModule.setup(
+      'specz',
+      app,
+      SwaggerModule.createDocument(
+        app,
+        new DocumentBuilder()
+          .setTitle('Parking Lot')
+          .setVersion('1.0')
+          .addTag('parking lot')
+          .build(),
+      ),
+    );
+  }
+
+  await app.listen(80);
 }
+
 bootstrap();
